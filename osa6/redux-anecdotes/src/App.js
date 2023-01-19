2bf59e9eea67e2124d4a35e38325beafbf7ae4f7
Ville Räsänen <ville.rasanen@hotmail.com>
2023-01-19 14:30:07 +0200

    6.8

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,34 +1,15 @@
-import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm';
+import AnectodeList from './components/AnecdotesList';
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
-
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            <AnectodeList />
+            <AnecdoteForm />
         </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default App
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,29 +1,61 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
+    return {
+        content: anecdote,
+        id: getId(),
+        votes: 0
+    }
 }
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+const anecdotesReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'VOTE': {
+            const votedAnecdote = state.find(anecdote => anecdote.id === action.data.id)
+            const switchAnecdote = {
+                ...votedAnecdote, votes: votedAnecdote.votes + 1
+            }
 
-  return state
+            const tempState = state.map(anecdote => anecdote.id !== action.data.id ? anecdote : switchAnecdote)
+            return tempState.sort((a, b) => b.votes - a.votes)
+        }
+        case 'ADD': {
+            const newAnecdote = {
+                content: action.data.content,
+                id: getId(),
+                votes: 0
+            }
+            return state.concat(newAnecdote)
+        }
+        default:
+            return state
+    }
+
 }
 
-export default reducer
+export const voteAnecdote = (id) => {
+    return {
+        type: 'VOTE',
+        data: {id}
+    }
+}
+
+export const addAnecdote = (content) => {
+    return {
+        type: 'ADD',
+        data: {content}
+    }
+}
+
+export default anecdotesReducer
